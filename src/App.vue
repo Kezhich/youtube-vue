72a45ef5b93da490df96fe56861675d48973b0f2
Kezhich <azerot96@gmail.com>
2019-04-14 15:02:34 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <SearchBox @termChange="onTermChange"></SearchBox>
+    <div class="row">
+    <VideoDetail :video="selectedVideo"/>
+    <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import SearchBox from "./components/SearchBox.vue";
+import VideoList from "./components/VideoList.vue";
+import VideoDetail from "./components/VideoDetail.vue";
+const API_KEY = "AIzaSyCA5HZ2xnJ9Wz7Aw9R406EU98oHgL7wTWs";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    SearchBox,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null
+    };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
+    },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @input="onInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "searchbox",
+  methods: {
+    onInput: function(e) {
+      this.$emit("termChange", event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  width: 75%;
+}
+
+div {
+  text-align: center;
+  margin: 20px;
+}
+</style>
